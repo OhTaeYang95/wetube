7893f284be38c59a6643b1a6eb265ca4636160aa
OhTaeYang95 <66006154+OhTaeYang95@users.noreply.github.com>
2021-01-14 10:56:18 +0900

    01.14-update01

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,31 @@
-export const join = (req, res) => res.render("join",{pageTitle : "Join"});
-export const login = (req, res) => res.render("login",{pageTitle : "Login"});
-export const logout = (req, res) => res.render("logout",{pageTitle : "Log Out"});
+import routes from "../routes";
+
+export const getJoin = (req, res) =>
+ res.render("join",{pageTitle : "Join"});
+
+ export const postJoin = (req, res) =>{
+     const {
+         body: { name, email, password, password2}
+     } = req;
+
+     if(password !== password2){
+         res.status(400);
+         res.render("join",{pageTitle : "Join"});
+     } else{
+         // To Do : Register User(할일 : 사용자 등록)
+         // To Do : Log User In(할일 : 사용자 로그인)
+         res.redirect(routes.home);
+     }
+
+    };
+export const getLogin = (req, res) => res.render("login",{pageTitle : "Login"});
+export const postLogin = (req, res) =>{
+     res.redirect(routes.home);
+}
+export const logout = (req, res) =>{
+    // To Do: Process Log Out 로그아웃 처리
+    res.redirect(routes.home);
+} 
 export const users = (req, res) => res.render("users",{pageTitle : "Users"});
 export const userDetail = (req, res) => res.render("userDetail",{pageTitle : "User Detail"});
 export const editProfile = (req, res) => res.render("editProfile",{pageTitle : "Edit Profile"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,13 +1,30 @@
-export const home = (req, res) => res.render("home", {pageTitle : "Home"});
-export const videos = (req, res) => res.render("videos",{pageTitle : "Videos"});
-export const upload = (req, res) => res.render("upload",{pageTitle : "Upload"});
-export const videoDetail = (req, res) => res.render("videoDetail",{pageTitle : "Video Detail"});
-export const editVideo = (req, res) => res.render("editVideo",{pageTitle : "Edit Video"});
-export const deleteVideo = (req, res) => res.render("deleteVideo",{pageTitle : "Delete Video"});
+import {videos} from "../db";
+import routes from "../routes";
+
+export const home = (req, res) => {
+    res.render("home", {pageTitle : "Home", videos});
+};
 
 export const search = (req, res) => {
     const {
         query : { term : searchingBy}
     } = req;
-    res.render("search",{pageTitle : "Search", searchingBy});
+    res.render("search",{pageTitle : "Search", searchingBy, videos});
 };
+
+export const getUpload = (req, res) => res.render("upload",{pageTitle : "Upload"});
+export const postUpload = (req, res) => {
+    const {
+        body : {
+            file,
+            title,
+            description
+        }
+     } = req;
+        // To Do : Video Upload And Save 비디오 업로드 및 저장
+    res.redirect(routes.videoDetail());
+}
+export const videoDetail = (req, res) => res.render("videoDetail",{pageTitle : "Video Detail"});
+export const editVideo = (req, res) => res.render("editVideo",{pageTitle : "Edit Video"});
+export const deleteVideo = (req, res) => res.render("deleteVideo",{pageTitle : "Delete Video"});
+
